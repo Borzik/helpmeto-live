a3e5c8249fcdfe9a0c3a198d1ea02b134f27243f
Felix Borzik <borzik1992@gmail.com>
2020-03-18 21:15:50 +0100

    fix not authorized error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
-  # rescue_from Pundit::NotAuthorizedError do
-  #   redirect_to root_path, error: 'You are not authorized to perform this action'
-  # end
+  rescue_from Pundit::NotAuthorizedError do
+    redirect_to root_path, error: 'You are not authorized to perform this action'
+  end
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
