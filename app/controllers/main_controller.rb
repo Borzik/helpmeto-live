ff98a84992a7dd88a90691c0a83458fa72ee7b97
Felix Borzik <borzik1992@gmail.com>
2020-03-20 14:12:09 +0100

    improve flow and make it more friendly

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
   skip_after_action :verify_authorized, only: %i[qa tos privacy_policy]
+  before_action :redirect_to_need, only: :index
+
   def index
   end
 
@@ -11,5 +13,11 @@ class MainController < ApplicationController
   end
 
   def privacy_policy
+  end
+
+  private
+  def redirect_to_need
+    return if !logged_in? || current_user.volunteer? || current_user.need.nil?
+    redirect_to my_need_path
   end
 end
